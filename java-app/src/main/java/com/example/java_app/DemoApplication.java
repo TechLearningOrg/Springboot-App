fef7e5af6f503a15f419a7bd6fa32efec70043bb
Prince Robin <princerobinraj@gmail.com>
2025-10-01 12:17:22 +0530

    Update DemoApplication.java

--- java-app/src/main/java/com/example/java_app/DemoApplication.java
+++ java-app/src/main/java/com/example/java_app/DemoApplication.java
@@ -14,6 +14,6 @@ public class DemoApplication {   // class name matches file
 
     @GetMapping("/")
     public String hello() {
-        return "Hello World from Java Spring Boot";
+        return "Hello World from prince and Java Spring Boot";
     }
 }
